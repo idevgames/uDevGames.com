57b9181e387c0de5f95912f651a8286305a26c45
Christopher R. Miller <xpm@mysteriouspants.com>
2020-10-10 22:35:45 -0700

    Remove unused file
    Worse, it was confusing because it was old and not updated.

--- dotenv.sh
+++
@@ -1,9 +0,0 @@
-# copy this to .env and fill in the missing bits to begin hacking
-APP_DATABASE_PATH=db/app.sqlite # the sqlite database to store everything in
-UDEVGAMES_APP_WORKERS=16 # the number of worker threads backing this app
-UDEVGAMES_APP_ADDRESS=127.0.0.1 # the address to bind to
-UDEVGAMES_APP_PORT=4000 # the port to run on
-# get a new development application registered with github
-# https://github.com/settings/applications/new
-GH_CLIENT_ID=
-GH_CLIENT_SECRET=
