ffdba42f51b5b43f0d4ad4fbdb08220bfa416919
Christopher R. Miller <xpm@mysteriouspants.com>
2020-11-21 19:26:17 -0800

    Improve error handling on Attachment controller

--- src/controllers/attachments.rs
+++ src/controllers/attachments.rs
@@ -1,32 +1,9 @@
 use crate::{
-    attachments::{AttachmentStorage, AttachmentStorageError},
+    attachments::AttachmentStorage,
     db::DbPool,
     models::Attachment,
 };
-use rocket::{get, response::Stream, tokio::fs::File, Responder, State};
-
-/// Errors getting an attachment.
-#[derive(Debug, Responder)]
-pub enum GetAttachmentError {
-    /// No such attachment exists in record, or isn't public, at any rate.
-    #[response(status = 404)]
-    AttachmentNotFound(String),
-
-    /// Could not query the database for an attachment.
-    #[response(status = 500)]
-    DbError(String),
-
-    /// The attachment is more or less valid, but the file on disk isn't there.
-    #[response(status = 500)]
-    FileNotFound(String),
-
-    /// The attachment could not be read from disk. Weird.
-    IoError(String),
-
-    /// Could not get a connection to the database out of the pool.
-    #[response(status = 500)]
-    PoolError(String),
-}
+use rocket::{get, response::Stream, tokio::fs::File, State};
 
 // match stuff like udevgames.com/attachments/1/my_file.jpeg
 #[get("/attachments/<id>/<_name>")]
@@ -35,32 +12,12 @@ pub async fn get_attachment(
     attachment_storage: State<'_, AttachmentStorage>,
     id: i32,
     _name: String,
-) -> Result<Stream<File>, GetAttachmentError> {
-    let conn = match pool.get() {
-        Ok(conn) => conn,
-        Err(e) => {
-            return Err(GetAttachmentError::PoolError(format!("{:?}", e)))
-        }
+) -> Result<Stream<File>, super::HandlerError> {
+    let conn = pool.get()?;
+    let attachment = match Attachment::find_published_by_id(&conn, id)? {
+        Some(attachment) => attachment,
+        None => return Err(super::HandlerError::NotFound),
     };
-    let attachment = match Attachment::find_published_by_id(&conn, id) {
-        Ok(attachment) => match attachment {
-            Some(attachment) => attachment,
-            None => {
-                return Err(GetAttachmentError::AttachmentNotFound(
-                    "Not found. Send in Search and Rescue.".to_string(),
-                ))
-            }
-        },
-        Err(e) => return Err(GetAttachmentError::DbError(format!("{:?}", e))),
-    };
-    let f = match attachment_storage.load(attachment.id) {
-        Ok(f) => f,
-        Err(AttachmentStorageError::NotFound(path)) => {
-            return Err(GetAttachmentError::FileNotFound(format!("{:?}", path)))
-        }
-        Err(AttachmentStorageError::IoError(e)) => {
-            return Err(GetAttachmentError::IoError(format!("{:?}", e)))
-        }
-    };
+    let f = attachment_storage.load(attachment.id)?;
     Ok(Stream::from(File::from_std(f)))
 }
--- src/controllers/mod.rs
+++ src/controllers/mod.rs
@@ -3,3 +3,40 @@ pub mod gh_oauth;
 pub mod homepage;
 pub mod jam_entries;
 pub mod jams;
+
+use rocket::{Request, response::Responder, response::Result as RocketResult, http::Status};
+use thiserror::Error;
+
+/// Unified error type for most (all?) handlers. Puts all the annoying
+/// boilerplate of derives into one spot with a single implementation of
+/// Responder to map these back onto error codes that ought to go back and hit
+/// the right error catchers.
+///
+/// Note that it would be very tempting to use anyhow for this, however we
+/// cannot implement Responder for it. Even if we could, inferring the http
+/// status code from a Boxed error would be rather challenging.
+#[derive(Debug, Error)]
+pub enum HandlerError {
+    #[error("The resource was not found")]
+    NotFound,
+
+    #[error("Could not get a connection from the pool with error {0}")]
+    PoolError(#[from] diesel::r2d2::PoolError),
+
+    #[error("Failed to query the database with error {0}")]
+    DatabaseError(#[from] crate::models::ModelError),
+
+    #[error("Failed to store/retrieve attachment with error {0}")]
+    AttachmentStorageError(#[from] crate::attachments::AttachmentStorageError),
+}
+
+impl<'r, 'o: 'r> Responder<'r, 'o> for HandlerError {
+    fn respond_to(self, _request: &'r Request<'_>) -> RocketResult<'o> {
+        Err(match self {
+            HandlerError::AttachmentStorageError(_) => Status::InternalServerError,
+            HandlerError::DatabaseError(_) => Status::InternalServerError,
+            HandlerError::PoolError(_) => Status::InternalServerError,
+            HandlerError::NotFound => Status::NotFound,
+        })
+    }
+}
